93a5ad85d453cbf58f1037bcfc010e4fd5bb7554
zhangyingmax <419886246@qq.com>
2025-01-11 20:35:09 +0800

    1

---
+++ src/main/java/test_1.java
@@ -0,0 +1,51 @@
+public class test_1 {
+    public static void main(String[] args) {
+//        //定义一个byte类型的变量
+//        byte a=12;
+//        System.out.println(a);
+//        //定义一个short类型的变量
+//        short b;
+//        b=123;
+//        System.out.println(b);
+        //定义一个int类型的变量
+//        int c=1000000000;
+//        System.out.println(c);
+        //定义一个long类型的变量
+//        long d=200000000000L;
+//        System.out.println(d);
+        //定义一个float类型的变量
+//        float e =3.14F;
+//        System.out.println(e);
+//        //定义一个double类型的变量
+//        double f=3.14;
+//        System.out.println(f);
+//        //定义一个char类型的变量
+//        char g='g';
+//        System.out.println(g);
+//        //定义一个boolean类型的变量
+//        boolean h=true;
+//        boolean i=false;
+//        System.out.println(h);
+//        System.out.println(i);
+
+//        int c=1;
+//        c=2;
+//        System.out.println(c);
+
+        //强制转换
+//        int i=130;
+//        byte b=(byte)i;
+//        System.out.println(b);
+
+        int i=111;
+        String c="avd";
+//        int d=a+b;
+        System.out.println(c);
+//        System.out.println(d);
+
+
+
+    }
+
+
+}
